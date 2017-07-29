eef322a7d3ca908927410e6fd8f85c7d79fee7f8
潘前进 <panqianjin@panqianjindeMacBook-Pro.local>
2017-07-29 16:20:02 +0800

    test

--- index.js
+++ index.js
@@ -1,20 +1,8 @@
 var Logger = require('pmlogger')
-var test = function () {
-    var loggerOpt = {
-        //组件id统一为1
-        id: 1,
-        //上报地址
-        url: 'http://future.test.sankuai.com/monitor/logstore/log/',
-        // 浏览器端上传
-        attribute: 1
-    }
-    window.pmlogger = window.pmlogger || new Logger(loggerOpt)
-    pmlogger.info({
-        attribute: 1,
-        type: 8,
-        pageUrl: location.origin+location.pathname
-    })
-}
+
 module.exports = {
-    test:test
+    test:function(){
+        let a = Logger.ModuleLogger
+        new a('测试')
+    }
 }
--- scripts.js
+++ scripts.js
@@ -1,12 +1,16 @@
 var request = require('superagent')
-var p = require('./package.json')
-var P = require('../../package.json')
+var p = require('../package.json')
+var P = require('../../../package.json')
 var path = require('path')
-console.log('项目名：' + P.name + '>>>>>>>>组件名:' + p.name)
-console.log(__dirname)
-request
-  .get('http://future.test.sankuai.com/monitor/logstore/log/')
-  .query({ attribute: '0', componentName: p.name, componentVersion: p.version, appName: P.name, type: 'component' })
-  .end(function (err, res) {
+try {
+  console.log('项目名：' + P.name + '>>>>>>>>组件名:' + p.name)
+  console.log(__dirname)
+  request
+    .get('http://future.test.sankuai.com/monitor/logstore/log/')
+    .query({ attribute: '0', componentName: p.name, componentVersion: p.version, appName: P.name, type: 'component' })
+    .end(function (err, res) {
 
-  });
+    });
+} catch (e) {
+  console.log(e)
+}
